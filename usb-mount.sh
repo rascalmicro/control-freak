3220ad4027308094bc85cfe67d54aa5c0231402d
Brandon Stafford <brandon@pingswept.org>
2011-03-28 18:51:07 -0400

    Remove unused files

--- usb-mount.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-umount -d /dev/loop0
-losetup -d /dev/loop0 
-modprobe g_file_storage file=/root/usb_mass_storage_dev_backing.bin
-
-losetup -o 4096 /dev/loop0 /root/usb_mass_storage_dev_backing.bin
