189def84311d8f6c11eb2c59b5432bedd84864a5
Brandon Stafford <brandon@pingswept.org>
2011-08-17 02:46:41 +0000

    Fix editor saving and take first cut at log and monitor pages

--- editor/__init__.py
+++ editor/__init__.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, request, abort
+from flask import abort, Blueprint, redirect, render_template, request
 from jinja2 import TemplateNotFound
 from werkzeug import secure_filename
 
-editor = Blueprint('editor', __name__, static_folder='static', template_folder='templates')
+editor = Blueprint('editor', __name__, static_url_path='/editor/', static_folder='static', template_folder='templates')
 
 def secure_path(path): # Version of Werkzeug's secure_filename trimmed to allow paths through (could be a bad idea)
     if isinstance(path, unicode):
@@ -10,24 +10,61 @@ def secure_path(path): # Version of Werkzeug's secure_filename trimmed to allow 
         path = normalize('NFKD', path).encode('ascii', 'ignore')
     return path
 
+def tail(f, n, offset=None):
+    """Reads a n lines from f with an offset of offset lines.  The return
+    value is a tuple in the form ``(lines, has_more)`` where `has_more` is
+    an indicator that is `True` if there are more lines in the file.
+    From Armin Ronacher on Stack Overflow: http://stackoverflow.com/questions/136168/get-last-n-lines-of-a-file-with-python-similar-to-tail/692616#692616
+    """
+    avg_line_length = 74
+    to_read = n + (offset or 0)
+
+    while 1:
+        try:
+            f.seek(int(-(avg_line_length * to_read)), 2)
+        except IOError:
+            # woops.  apparently file is smaller than what we want
+            # to step back, go to the beginning instead
+            f.seek(0)
+        pos = f.tell()
+        lines = f.read().splitlines()
+        if len(lines) >= to_read or pos == 0:
+            return lines[-to_read:offset and -offset or None], \
+                   len(lines) > to_read or pos > 0
+        avg_line_length *= 1.3
+
 @editor.route('/config')
 def config():
     try:
-        return render_template('config.html')
+        import subprocess
+        process_table = subprocess.Popen('ifconfig', stdout=subprocess.PIPE)
+        return render_template('config.html', processes=process_table.communicate()[0])
     except TemplateNotFound:
         abort(404)
 
 @editor.route('/log')
 def log():
     try:
-        return render_template('log.html')
+        f = open('/var/log/uwsgi.log', 'r')
+        app_log = '</td></tr>\n<tr><td>'.join(tail(f, 10)[0])
+        f.close()
+        f = open('/var/log/nginx/access', 'r')
+        access_log = '</td></tr>\n<tr><td>'.join(tail(f, 10)[0])
+        f.close()
+        f = open('/var/log/nginx/error', 'r')
+        error_log = '</td></tr>\n<tr><td>'.join(tail(f, 10)[0])
+        f.close()
+        return render_template('log.html', app=app_log, access=access_log, error=error_log)
     except TemplateNotFound:
         abort(404)
 
 @editor.route('/monitor')
-def log():
+def monitor():
     try:
-        return render_template('monitor.html')
+        import subprocess
+        process_list = subprocess.Popen('ps', stdout=subprocess.PIPE).communicate()[0].split('\n')
+        table = '</td></tr>\n<tr><td>'.join(process_list).replace(' ', '&nbsp;')
+        return render_template('monitor.html', processes=table)
     except TemplateNotFound:
         abort(404)
 
@@ -38,8 +75,8 @@ def start_edit():
     except TemplateNotFound:
         abort(404)
 
-@editor.route('/edit/<path:path>')
-def edit(path):
+@editor.route('/edit/<path:path>', methods=['GET', 'POST']) # POST added because
+def edit(path):                            # of redirects from /save route POSTs
     try:
         path = secure_path(path)
         f = open('/var/www/public/' + path, 'r')
@@ -83,8 +120,8 @@ def reload():
 @editor.route('/save', methods=['POST'])
 def save():
     root = '/var/www/public/'
-    path = request.form['path']
-    f = open(root + secure_path(str(path)), 'w')
+    path = secure_path(request.form['path'])
+    f = open(root + path, 'w')
     f.write(request.form['text'])
     f.close()
-    return redirect('/edit', 200)
+    return redirect('/edit/' + path, 302)
