f72b30ad2a8172395e8e04f149089464cf48572a
root <root@rascal14.(none)>
2013-03-26 21:36:02 +0000

    Remove spurious test file

--- public/crap.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, request
-public = Blueprint('crap', __name__, template_folder='templates')
-
-@public.route('/example/<variable>')
-def example(variable):
-    try:
-        return variable * request.form['data']
-    except:
-        return variable * data
